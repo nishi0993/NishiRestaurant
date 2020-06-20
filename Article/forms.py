5db37025c2ace5cb1975933f3721643ba8ee3120
nishi0993 <nishi.chaudhary_ccv17@gla.ac.in>
2020-06-20 22:31:09 +0530

    label_suffix and help_text usage

--- Article/forms.py
+++ Article/forms.py
@@ -1,11 +1,12 @@
 from django.forms import ModelForm
 from django import forms
+import datetime
 from Article.models import Author, Book
 
 class Comment(forms.Form):
-    name = forms.CharField(label='Your Name')
-    email = forms.EmailField(label='Email', required=False)
-
+    name = forms.CharField(label='Your Name', label_suffix=' = ', help_text='Full')
+    email = forms.EmailField(label='Email', required=False, initial='www.')
+    date = forms.DateField(initial=datetime.date.today)
 
 class AuthorForm(ModelForm):
     class Meta:
